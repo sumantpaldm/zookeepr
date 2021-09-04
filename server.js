9e523408308d17dad22037e577cf28638971581b
SINGH 1258 <sumantpaldm@gmail.com>
2021-09-03 20:19:26 -0400

    add heroku port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { animals } = require('./data/animal.json');
 
-
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -51,6 +51,6 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
     console.log(`API server now on port 3001!`);
 });
